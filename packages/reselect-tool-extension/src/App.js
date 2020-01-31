51390377c9e66cafd101c09fd28b04cd8ab16ce6
Andrea Dito <Andrea.Dito@thetrainline.com>
2020-01-31 17:39:08 +0000

    fix deps

--- packages/reselect-tool-extension/src/App.js
+++ packages/reselect-tool-extension/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { TreeList, Column, SearchPanel, ColumnChooser, Scrolling, Paging, Pager, HeaderFilter } from 'devextreme-react/tree-list';
+
 export default class App extends Component {
 
   flatSelectors(selectors) {
@@ -26,6 +27,8 @@ export default class App extends Component {
           id="selectors"
           dataSource={selectors}
           defaultExpandedRowKeys={[1]}
+          focusedRowEnabled={true}
+          focusedRowKey={0}
           showRowLines={true}
           showBorders={true}
           columnAutoWidth={true}
@@ -59,6 +62,19 @@ export default class App extends Component {
           />
           <ColumnChooser enabled={true} mode="select" />
         </TreeList>
+        {
+          <div className="task-info">
+            <div className="info">
+              <div className="task-subject"></div>
+              <span className="task-assigned"></span>
+              <span className="start-date"></span>
+            </div>
+            <div className="progress">
+              <span className="task-status"></span>
+              <span className="task-progress"></span>
+            </div>
+          </div>
+        }
       </div>
     );
   }
